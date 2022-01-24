034f1b23d3f9a6100b9922a19c7bc917af265973
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2022-01-25 00:08:03 +0200

    Add  struct Constants and an url that I will work with

---
+++ HTMLParsing/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  HTMLParsing
+//
+//  Created by MacBook on 25.01.2022.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let url = "https://www.avenga.com/career/ukraine/all-openings/"
+}
